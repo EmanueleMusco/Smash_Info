4cca91819e6f5f4fdfef1900d83a5f3e2f21e251
Emanuele <emanuelemusco003@gmail.com>
2022-04-07 23:08:43 +0200

    musica finita

--- script.js
+++ script.js
@@ -5,14 +5,24 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext('2d');
 let audioSource;
 let analyser;
-
+let k = 0;
+let cur_time = 0;
+let arr = [
+    'audio/J. Cole - p r i d e . i s . t h e . d e v i l  feat. Lil Baby (Official Audio).mp3', 
+    'audio/Paky - Vita Sbagliata .mp3', 
+    'audio/Rhove - Cancelo.mp3'
+];
 c = 0;
+
 button5.addEventListener('click', function(){
     c++;
+    audio1.volume=0.5;
     if(c == 1){
         const audio1 = document.getElementById('audio1');
-        audio1.src = 'audio/J. Cole - p r i d e . i s . t h e . d e v i l  feat. Lil Baby (Official Audio).mp3';
+        let myaudio = document.getElementById("audio1")[0];
+        audio1.src = play();
         const audioContext = new AudioContext();
+        audio1.currentTime = cur_time;
         audio1.play();
         button5.innerHTML="Disattiva musica";
         audioSource = audioContext.createMediaElementSource(audio1);
@@ -22,11 +32,11 @@ button5.addEventListener('click', function(){
         analyser.fftSize = 256;
         const bufferLenght = analyser.frequencyBinCount;
         const dataArray = new Uint8Array(bufferLenght );
-    
+
         const barWidth = canvas.width/bufferLenght;
         let barHeight;
         let x;
-    
+
         function animate(){
             x = 0;
             ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -40,30 +50,34 @@ button5.addEventListener('click', function(){
             requestAnimationFrame(animate);
         }
         animate();
-    }else{
-        audio1.pause();
+    }else {
+        audio1.pause()
+        cur_time = audio1.currentTime;
         button5.innerHTML="Attiva Musica";
         c = 0;
     }
 
 });
-
 //ripetizione playlist
-function cambiaCanzone(){
-    k = 0;
-    let arr = [
-        'audio/J. Cole - p r i d e . i s . t h e . d e v i l  feat. Lil Baby (Official Audio).mp3', 
-        'audio/Paky - Vita Sbagliata .mp3', 
-        'audio/Rhove - Cancelo.mp3'
-    ];
-    audio1.onended = function(){
-        k++;
-        audio1.src = arr[k];
-        audio1.play();
-    };
+function play(){
+
+    audio1.src = arr[k];
+    return audio1.src
 }
 
-cambiaCanzone();
+audio1.onended = function(){
+    if(k != arr.length -1)
+    {
+        k++
+        audio1.src = play()
+        audio1.play();
+    }else{
+        k = 0
+        audio1.src = play()
+        audio1.play();
+    }
+};
+
 //------------------//
 
 
